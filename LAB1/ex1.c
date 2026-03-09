983c162764ed75cd09f8785cf7f9824cffe492db
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-03-09 00:26:15 +0000

    atualizando o codigo

--- LAB1/ex1.c
+++ LAB1/ex1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    printf("hello world\n");
+float foo (float pf) {
+  return pf+1;
+}
 
-    return 0;
+int main (int argc, char **argv) {
+  float f;
+  if (argc!=2) {
+    printf ("uso do programa: %s <valor float>\n", argv[0]);
+    exit(0);
+  }
+  f = atof(argv[1]);
+  printf ("foo(%.2f): %.2f\n", f, foo(f));
+  return 0;
 }
