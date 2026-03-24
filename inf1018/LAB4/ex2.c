12daa9bad5e61090650c906593965973d97b02bd
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-03-24 17:44:43 +0000

    ex2

--- inf1018/LAB4/ex2.c
+++ inf1018/LAB4/ex2.c
@@ -18,3 +18,17 @@ int main (void) {
   dump(&k, sizeof(k));
   return 0;
 }
+
+/*
+saida esperada:
+dump de l: 
+0x7ff... - 03
+0x7ff...f1 - 80
+dump de k: 
+0x7ff... - 03
+0x7ff... - 80
+
+Ao fazer p1++, você está andando 
+exatamente 1 byte de cada vez na memória, revelando
+ como o short (que tem 2 bytes) está deitado nos endereços físicos.
+*/
