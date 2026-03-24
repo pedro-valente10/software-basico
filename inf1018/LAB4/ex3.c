3f21c505ec9dfe04a0057aa73717d71f884911cd
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-03-24 18:03:07 +0000

    terceira

--- inf1018/LAB4/ex3.c
+++ inf1018/LAB4/ex3.c
@@ -27,18 +27,12 @@ int string2num (char *s, int base) {
   return a;
 }
 
-int xbyte (packed_t word, int bytenum) {
-  /* implementar!!! */
-  return 1;
-}
-
 int main (int argc, char **argv) {
   int x;
   if (argc != 3) {
     printf ("uso: %s <word (em hexadecimal)> <bytenum>\n", argv[0]);
     exit(1);
   }
-
   x = xbyte(string2num(argv[1], 16), atoi(argv[2]));
   printf ("%08x  %d\n", x, x);
   return 0;
