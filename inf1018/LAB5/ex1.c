3d9302a7eb8c1294aa3ce9a7e053dc7218542f75
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-04-06 21:06:43 +0000

    ex1 lab5

---
+++ inf1018/LAB5/ex1.c
@@ -0,0 +1,31 @@
+/*int b[2]; deve ter dois inteiros, então 8 bytes
+short a[2][3]; deve ter 6 shorts, então 12 bytes*/
+
+#include <stdio.h>
+
+void dump (void *p, int n) {
+  unsigned char *p1 = p;
+  while (n--) {
+    printf("%p - %02x\n", p1, *p1);
+    p1++;
+  }
+}
+
+int main (void) {
+  int i,j;
+  short a[2][3];
+  int b[2];
+
+  for (i=0;i<2;i++) {
+    b[i] = i;
+    for (j=0;j<3;j++)
+      a[i][j] = 3*i+j;
+  }
+
+  printf ("b: \n");
+  dump (b, sizeof(b));
+  printf ("a: \n");
+  dump (a, sizeof(a));
+
+  return 0;
+}
