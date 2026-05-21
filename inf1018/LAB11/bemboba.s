314ff32310a7521d290720060a774d9127847f85
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-05-21 12:49:25 +0000

    conclusao

--- inf1018/LAB11/bemboba.s
+++ inf1018/LAB11/bemboba.s
@@ -17,13 +17,8 @@
 bemboba:
     pushq   %rbp
     movq    %rsp, %rbp
-
-    # --- Alocação de Espaço na Pilha ---
-    # local[4] precisa de 16 bytes (4 ints * 4 bytes).
-    # Subtraímos 16 do %rsp para abrir esse espaço.
     subq    $16, %rsp       
 
-    # --- Inicialização do Loop ---
     movl    $0, %ecx
     leaq    -16(%rbp), %rdx
 
@@ -31,18 +26,15 @@ bemboba:
     cmpl    $4, %ecx
     jge     .L_loop_end
 
-    # --- Corpo do Loop ---
     movl    %edi, (%rdx)
     addq    $4, %rdx 
     incl    %ecx
     jmp     .L_loop
 
 .L_loop_end:
-    # --- Preparação para chamar addl(local, 4) ---
     leaq    -16(%rbp), %rdi
     movl    $4, %esi
     call    addl
 
-    # --- Finalização da Função ---
     leave
     ret
